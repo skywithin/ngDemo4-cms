896f3088900685af1519967037ae85bfc7f55be0
Max Vagner <sydneymax@gmail.com>
2018-04-02 22:46:27 +1000

    Add login logic

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginFailed: boolean = false;
+  userRegistered: boolean = false;
+  displayMsgTimeout: number = 2000;
+
+  constructor(
+    private router: Router,
+    private userService: UserService
+  ) { }
 
   ngOnInit() {
+    if (localStorage.getItem("userRegistered")) {
+      this.userRegistered = true;
+      localStorage.removeItem("userRegistered");
+    }
   }
 
+  login({value, valid}){
+    if (valid) {
+      this.userService.login(value).subscribe(response => {
+        if (response == 'invalidLogin') {
+          this.loginFailed = true;
+          setTimeout(function() {
+            this.loginFailed = false;
+          }.bind(this), this.displayMsgTimeout)
+        } else {
+          // Success
+          localStorage.setItem("user", JSON.stringify(response));
+
+          if (localStorage.getItem("user") === "\"admin\"") {
+            this.router.navigateByUrl('admin/pages');
+          } else {
+            this.router.navigateByUrl('');
+          }
+        }
+      });
+    } else {
+      console.log('Form is not valid.')
+    }
+  }
 }
