4a9bb8827d479b4a376edd71bc038c80d8e70c30
Max Vagner <sydneymax@gmail.com>
2018-04-02 21:22:56 +1000

    Admin Sidebar 02

--- src/app/components/admin-sidebar/admin-sidebar.component.ts
+++ src/app/components/admin-sidebar/admin-sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SidebarService } from '../../services/sidebar.service';
 
 @Component({
   selector: 'app-admin-sidebar',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminSidebarComponent implements OnInit {
 
-  constructor() { }
+  content: string;
+  id: string;
+  successMsg: boolean;
+  displayMsgTimeout: number = 2000;
+  
+  constructor(
+    private router: Router,
+    private sidebarService: SidebarService
+  ) { }
 
   ngOnInit() {
+    this.sidebarService.getSidebar().subscribe(response => {
+      this.content = response["content"];
+      this.id = response["id"];
+    });
+  }
+
+  editSidebar({value}){
+    this.sidebarService.putEditSidebar(value).subscribe(response => {
+      // Success
+      this.successMsg = true;
+      setTimeout(function() {
+        this.successMsg = false;
+      }.bind(this), this.displayMsgTimeout)
+    });
   }
 
 }
