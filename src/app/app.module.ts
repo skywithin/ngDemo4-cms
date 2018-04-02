b564983a4c2066b665bc7115e05fb4fab0c05635
Max Vagner <sydneymax@gmail.com>
2018-04-02 13:13:48 +1000

    Routing for Pages component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { PageService } from './services/page.service';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { PagesComponent } from './components/pages/pages.component';
+
+const appRoutes: Routes = [
+  {
+    path: ':page',
+    component: PagesComponent
+  },
+  {
+    path: '',
+    component: PagesComponent
+  }
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +28,8 @@ import { PagesComponent } from './components/pages/pages.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     PageService
