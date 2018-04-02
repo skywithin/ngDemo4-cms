1b2fa07c8a9ec3fcb208366f09df9a10f99373e2
Max Vagner <sydneymax@gmail.com>
2018-04-02 13:01:37 +1000

    Add Pages component logic

--- src/app/components/pages/pages.component.ts
+++ src/app/components/pages/pages.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { PageService } from '../../services/page.service';
 
 @Component({
   selector: 'app-pages',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagesComponent implements OnInit {
 
-  constructor() { }
+  private param: any;
+  private pageBody: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private title: Title,
+    private pageService: PageService
+  ) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.param = params['page'];
+
+      if (this.param === undefined) {
+        this.param = 'home';
+        this.title.setTitle('CMS');
+      } else {
+        this.title.setTitle(this.param.replace(/-/g, ' ').replace(/\b\w/g, l => l.toUpperCase()));
+      }
+
+      this.pageService.getPage(this.param).subscribe(page => {
+        if (page == "PageNotFound") {
+          this.router.navigateByUrl('');
+        }
+        this.pageBody = page["content"];
+      });
+    });
   }
 
 }
